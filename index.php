52ff6408d97add03f23149cd84949b885a0387b9
shibatests <shiba.fyp@gmail.com>
2021-08-30 16:04:13 +0200

    make it better

--- index.php
+++ index.php
@@ -1,3 +1,54 @@
-<?php 
-echo"hello SHIBASAKI"
+<?php
+
+//this is other comment
+//read the variables read from post
+$sessionId=$_POST["sessionId"];
+$serviceCode=$_POST["serviceCode"];
+$phoneNumber=$_POST["phoneNumber"];
+$text=$_POST["text"];
+
+if($text==""){
+
+    //this is the first request to
+    $response="CON what would you like to check \n ";
+    $response .= "1.My account \n";
+    $response .= "2.my phone number \n";
+
+
+}
+
+else if($text=="1"){
+
+    $response="CON chose account information";
+    $response .= "1.Account Number \n";
+    $response .= "2.Account balance ";
+
+}
+
+else if($text=="2"){
+    $response="END your phone number is ".$phoneNumber;
+
+}
+
+elseif($text=="1*1"){
+
+    //this  is the second level response that
+    $accountNumber="ACC1001";
+
+
+    $response="END your account number is ".$accountNumber;
+
+
+}
+elseif($text=="1*2")
+{
+
+    $balance="KES 10,000";
+    $response="END YOUR account balance is".$balance;
+
+}
+
+header('content-type;text/plain');
+echo $response;
+
 ?>
