84e84b76bbbedbf5b08b1704d1415706652fe95f
vanson250 <nkurunzizavanson@gmail.com>
2021-09-13 21:45:08 +0200

    modified script

--- index.php
+++ index.php
@@ -3,33 +3,30 @@
  * USSD gateway that is being used is Africa's Talking USSD gateway
  */
 
+// Print the response as plain text so that the gateway can read it
 header('Content-type: text/plain');
 
 /* local db configuration */
-// $dsn = 'bnw530k7urgmxgzkeziw;host=bnw530k7urgmxgzkeziw-mysql.services.clever-cloud.com'; //database name
-// $user = 'uuvo090e1awwwfz0'; // your mysql user 
-// $password = 'WknalOFgRERGk4rldEsr'; // your mysql password
-
-// $uri='mysql://uuvo090e1awwwfz0:WknalOFgRERGk4rldEsr@bnw530k7urgmxgzkeziw-mysql.services.clever-cloud.com:3306/bnw530k7urgmxgzkeziw';
+$dsn = 'mysql:dbname=bnw530k7urgmxgzkeziw;host=bnw530k7urgmxgzkeziw-mysql.services.clever-cloud.com;'; //database name
+$user = 'uuvo090e1awwwfz0'; // your mysql user 
+$password = 'WknalOFgRERGk4rldEsr'; // your mysql password
 
 //  Create a PDO instance that will allow you to access your database
 try {
-    $dbh = new PDO('mysql:host=bnw530k7urgmxgzkeziw-mysql.services.clever-cloud.com;dbname=bnw530k7urgmxgzkeziw', 'uuvo090e1awwwfz0', 'WknalOFgRERGk4rldEsr');
-}
-catch(PDOException $e) {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
     //var_dump($e);
-    echo($e." PDO error occurred");
-}
-catch(Exception $e) {
+    echo ("PDO error occurred");
+} catch (Exception $e) {
     //var_dump($e);
-    echo("Error occurred");
+    echo ("Error occurred");
 }
 
 // Get the parameters provided by Africa's Talking USSD gateway
-$phone = $_POST['phoneNumber'];
-$session_id = $_POST['sessionId'];
-$service_code = $_POST['serviceCode'];
-$ussd_string= $_POST['text'];
+$phone = $_GET['phoneNumber'];
+$session_id = $_GET['sessionId'];
+$service_code = $_GET['serviceCode'];
+$ussd_string = $_GET['text'];
 
 //set default level to zero
 $level = 0;
@@ -39,35 +36,34 @@ $level = 0;
  * One needs to split the response from Africa's Talking in order to determine
  * the menu level and input for each level
  * */
-$ussd_string_exploded = explode ("*",$ussd_string);
+$ussd_string_exploded = explode("*", $ussd_string);
+
 // Get menu level from ussd_string reply
-// $level = count($ussd_string_exploded);
-$level = $ussd_string;
-if($level == ""){
-    
+$level = count($ussd_string_exploded);
+
+if ($level == 1 or $level == 0) {
+
     display_menu(); // show the home/first menu
 }
 
-    else if ($level == "1")
-    {
-        // If user selected 1 send them to the registration menu
-        register($level,$phone, $dbh);
-    }
+if ($level > 1) {
 
-  else if ($level == "2"){
+    if ($ussd_string_exploded[0] == "1") {
+        // If user selected 1 send them to the registration menu
+        register($ussd_string_exploded, $phone, $dbh);
+    } else if ($ussd_string_exploded[0] == "2") {
         //If user selected 2, send them to the about menu
         about($ussd_string_exploded);
     }
-    else if($level == "1*") {
-        register($level,$phone, $dbh);
-    }
+}
 
 /* The ussd_proceed function appends CON to the USSD response your application gives.
  * This informs Africa's Talking USSD gateway and consecuently Safaricom's
  * USSD gateway that the USSD session is till in session or should still continue
  * Use this when you want the application USSD session to continue
 */
-function ussd_proceed($ussd_text){
+function ussd_proceed($ussd_text)
+{
     echo "CON $ussd_text";
 }
 
@@ -76,7 +72,8 @@ function ussd_proceed($ussd_text){
  * USSD gateway that the USSD session should end.
  * Use this when you to want the application session to terminate/end the application
 */
-function ussd_stop($ussd_text){
+function ussd_stop($ussd_text)
+{
     echo "END $ussd_text";
 }
 
@@ -96,40 +93,35 @@ function about($ussd_text)
 }
 
 // Function that handles Registration menu
-function register($details,$phone, $dbh){
-    if($details == "1")
-    {
+function register($details, $phone, $dbh)
+{
+    if (count($details) == 2) {
 
         $ussd_text = "Please enter your Full Name and Email, each seperated by commas:";
         ussd_proceed($ussd_text); // ask user to enter registration details
     }
-    // if($details== "1*2")
-    else
-    {
-        if (empty($details)){
-                $ussd_text = "Sorry we do not accept blank values";
-                ussd_stop($ussd_text);
-        } else {
-        $input = explode(",",$details);//store input values in an array
-        $full_name = $input[0];//store full name
-        $email = $input[1];//store email
-        $phone_number =$phone;//store phone number 
-
-        // build sql statement
-        $sth = $dbh->prepare("INSERT INTO customer1 (full_name, email, phone) VALUES('$full_name','$email','$phone_number')");
-        //execute insert query   
-        $sth->execute();
-        if($sth->errorCode() == 0) {
-            $ussd_text = $full_name." your registration was successful. Your email is ".$email." and phone number is ".$phone_number;
+    if (count($details) == 3) {
+        if (empty($details[1])) {
+            $ussd_text = "Sorry we do not accept blank values";
             ussd_proceed($ussd_text);
         } else {
-            ussd_stop($sth->errorInfo());
+            $input = explode(",", $details[1]); //store input values in an array
+            $full_name = $input[0]; //store full name
+            $email = $input[1]; //store email
+            $phone_number = $phone; //store phone number 
+
+            // build sql statement
+            $sth = $dbh->prepare("INSERT INTO customer1 (full_name, email, phone) VALUES('$full_name','$email','$phone_number')");
+            //execute insert query   
+            $sth->execute();
+            if ($sth->errorCode() == 0) {
+                $ussd_text = $full_name . " your registration was successful. Your email is " . $email . " and phone number is " . $phone_number;
+                ussd_proceed($ussd_text);
+            } else {
+                $errors = $sth->errorInfo();
+            }
         }
     }
 }
-}
 # close the pdo connection  
 $dbh = null;
-// header('Content-type: text/plain');
-// echo $response;
-?>
