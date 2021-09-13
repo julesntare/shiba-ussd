d68d8f6df7f76848018763cb0c57773ec1d85159
shibatests <shiba.fyp@gmail.com>
2021-09-13 12:55:18 +0200

    hello

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $ussd_string_exploded = explode ("*",$ussd_string);
 // Get menu level from ussd_string reply
 $level = count($ussd_string_exploded);
 
-if($level == 1 or $level == 0){
+if($level == 0){
     
     display_menu(); // show the home/first menu
 }
