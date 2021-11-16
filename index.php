50df09930e128be07a55000c97baa26e0e645eed
shibatests <shiba.fyp@gmail.com>
2021-11-16 17:55:59 +0200

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $networkCode = $_POST['networkCode'];
 
 
 
-if($userinput=="*662*800*100#"){
+if($userinput=="*662*800*70#"){
    $response  = "Welcome to XYZ Cleaning Company\n";
    $response .="1. Register\n 2.Check your balance";
    
@@ -17,14 +17,14 @@ $ContinueSession=1;
 
 }
 
-elseif($userinput=="*662*800*100*1#"){
+elseif($userinput=="*662*800*700*1#"){
 
 $response  = "Your phone number is registered now ,thank you for choossing XYZ Cleaning Company";
 
 $ContinueSession=0;
 }
 
-elseif($userinput=="*662*800*100*2#"){
+elseif($userinput=="*662*800*700*2#"){
   $response  = "Enter Your House Number";
   $ContinueSession=1;
 
