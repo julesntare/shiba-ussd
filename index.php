930a7658fa837f8c8fb684b2dcc77667ef49377a
shibatests <shiba.fyp@gmail.com>
2021-11-16 18:23:02 +0200

    test

--- index.php
+++ index.php
@@ -326,3 +326,6 @@ function register($details, $phone, $dbConn)
 
 # close the pdo connection
 $dbConn = null;
+$resp = array("sessionId"=>$sessionId,"message"=>$response,"ContinueSession"=>$ContinueSession);
+
+  echo json_encode($resp); 
