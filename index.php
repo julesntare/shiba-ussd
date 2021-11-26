a30ab9b0f0c0b796c7f8390ae0bd999a3a815301
julesntare <julesntare@gmail.com>
2021-11-26 17:24:33 +0200

    added change in login func

--- index.php
+++ index.php
@@ -128,11 +128,11 @@ function login($level, $dbConn, $phone)
                 $res["status"] = 0;
             } else {
                 try {
-                    $p = str_replace("+", '%2B', $phone);
+                    $p = htmlspecialchars($phone);
                     $search_result = $dbConn->query("SELECT * FROM parents WHERE phone='$p' and pin='$pin'");
                     $total_rows = $search_result->rowCount();
                     if ($total_rows == 0) {
-                        $res["msg"] = "Umubare w'ibanga ntabwo ariwo.";
+                        $res["msg"] = $p . "\n" . $phone;
                         $res["status"] = 0;
                     } else {
                         $res["msg"] = "yesiii";
