eeb2536559f9751ff21fc6d09bea7149f1419db4
shibatests <shiba.fyp@gmail.com>
2021-12-10 10:52:21 +0200

    try

--- index.php
+++ index.php
@@ -165,8 +165,8 @@ function login($level, $dbConn, $phone)
                 $search_result_not = $dbConn->query("SELECT * FROM events");
                 $search_result_data = $search_result_not->fetchAll();
 
-                foreach($search_result_data as $x){
-                    $timetosend = $x[3];
+                foreach($search_result_data as $x => $y){
+                    $timetosend = $x;
                     $smstext = '123';
                     $dbConn->exec("INSERT INTO sms (receiver_phone, smstext, timetosend) VALUES('$pid', '$smstext', '$timetosend')");
                 }
