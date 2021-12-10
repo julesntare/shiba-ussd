af03c93f871d6bd68a775f08f744ea3ee05a23d6
shibatests <shiba.fyp@gmail.com>
2021-12-10 09:40:02 +0200

    try

--- index.php
+++ index.php
@@ -166,14 +166,14 @@ function login($level, $dbConn, $phone)
                 $res["msg"] = "Byegenze neza! " . $fname . " yanditswe muri sisitemu";
                 $res["status"] = 0;
                 
-                $search_result_not = $dbConn->query("SELECT * FROM events");
-                $search_result_not->fetchAll();
+                // $search_result_not = $dbConn->query("SELECT * FROM events");
+                // $search_result_not->fetchAll();
 
-                foreach($search_result_not as $key => $values){
-                    $timetosend = $values['period'] + time();
-                    $smstext = $values['message'];
-                    $dbConn->exec("INSERT INTO sms (receiver_phone, smstext, timetosend) VALUES('$pid', '$smstext', '$timetosend')");
-                }
+                // foreach($search_result_not as $key => $values){
+                //     $timetosend = $values['period'] + time();
+                //     $smstext = $values['message'];
+                //     $dbConn->exec("INSERT INTO sms (receiver_phone, smstext, timetosend) VALUES('$pid', '$smstext', '$timetosend')");
+                // }
             }
             break;
         default:
